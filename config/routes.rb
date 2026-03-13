0895f23be400c766135e43918df5974b10d4158b
Harshini <harshinisivapuram@gmail.com>
2026-03-13 10:12:00 +0000

    Updated Routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  post "book_seat/:seat_no", to: "seats#book"
-  post "cancel_seat/:seat_no", to: "seats#cancel"
+  get "book_seat/:seat_no", to: "seats#book"
+  get "cancel_seat/:seat_no", to: "seats#cancel"
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
